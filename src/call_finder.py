9bfdd79d44a4d8ada3e45a0558061ffcce8cc15e
Default user <efloch@locus.co>
2019-10-17 20:53:23 +0000

    l

--- src/call_finder.py
+++ src/call_finder.py
@@ -6,7 +6,7 @@ import locuspeerexplorer.peer_explorer as exp
 import locuspeerexplorer.peer_visualizer as vis
 import pandas as pd
 import os
-import geoviz.choropleth as choro
+# import geoviz.choropleth as choro
 
 df_data = pd.read_csv(
     "data/processed/metrics_outcomes.csv"
@@ -39,26 +39,36 @@ all_fms["None"] = None
 all_outcomes = {c: c for c in list(df_data.columns)[3:] if ("-" not in c)}
 all_outcomes["None"] = None
 
+MSA = 'New York'
+
+input_n_peers = widgets.IntSlider(value=5,min=0,max=20,step=1,description='# of Peers',orientation='horizontal')
+input_n_features = widgets.IntSlider(value=5,min=0,max=30,step=1,description='# of Features',orientation='horizontal')
+input_n_fms = widgets.IntSlider(value=5,min=0,max=30,step=1,description='# of FMs',orientation='horizontal')
+input_coverage = widgets.IntSlider(value=50,min=0,max=100,step=1,description='% of Empl',orientation='horizontal')
+input_year = widgets.RadioButtons(options=[2015, 2016],description='Year', disabled=False)
+input_msa = widgets.Dropdown(options=all_msas,value=name2code(MSA),description='MSA',layout=Layout(width="80%"))
+input_fms = widgets.SelectMultiple(options=all_fms,description='FM',layout=Layout(width="80%"))
+input_outcomes = widgets.SelectMultiple(options=all_outcomes,description='Outcome',layout=Layout(width="80%"))
 
 def show_peers(df_data, df_county_dist, df_msa_def, msa, n_peers, year):
     peers, fms = find.get_geographic_peers(
         df_data, df_county_dist, df_msa_def, msa, n_peers, year
     )
-    peers.append(msa)
+#     peers.append(msa)
     df_peers = pd.DataFrame({"MSA": [str(x) for x in peers]})
-    df_peers["MSA"] = df_peers["MSA"].astype(str)
-    df_peers["Name"] = df_peers["MSA"].apply(code2name)
+    df_peers["Peer MSA Code"] = df_peers["MSA"].astype(str)
+    df_peers["Peer Name"] = df_peers["MSA"].apply(code2name)
     df_peers["Is peer"] = 1
-    choro.plot(
-        df_peers,
-        "MSA",
-        "cbsa",
-        "Is peer",
-        "sequential",
-        formatting={"state_outline": "after"},
-    )
+#     choro.plot(
+#         df_peers,
+#         "MSA",
+#         "cbsa",
+#         "Is peer",
+#         "sequential",
+#         formatting={"state_outline": "after"},
+#     )
 
-    return df_peers
+    return df_peers[['Peer Name','Peer MSA Code']]
 
 
 def show_fms_peers(df_data, msa, year, n_peers, fms, outcomes):
@@ -73,8 +83,8 @@ def show_fms_peers(df_data, msa, year, n_peers, fms, outcomes):
     peers, fms = find.get_peers_from_input(df_data, msa, year, n_peers, fms, outcomes)
     for i in fms:
         vis.quadrant_viz(df_data, msa, [msa] + peers, i, save_fig=False, show=True)
-    df_peers = pd.DataFrame({"MSA": [str(x) for x in peers]})
-    df_peers["Name"] = df_peers["MSA"].apply(code2name)
+    df_peers = pd.DataFrame({"Peer MSA Code": [str(x) for x in peers]})
+    df_peers["Peer Name"] = df_peers["Peer MSA Code"].apply(code2name)
     return df_peers
 
 
@@ -83,18 +93,30 @@ def show_disting_peers(df_data, msa, year, n_peers, n_feat):
         df_data, msa, year, n_peers, n_feat
     )
     print(f"Comparison of {msa} and its peers for the 5 most distinguishing FMs")
-    for i in range(0, 5):
-        vis.quadrant_viz(df_data, msa, [msa] + peers, fms[i], save_fig=False, show=True)
-    df_peers = pd.DataFrame({"MSA": [str(x) for x in peers]})
-    df_peers["Name"] = df_peers["MSA"].apply(code2name)
+    for i in fms:
+        vis.quadrant_viz(df_data, msa, [msa] + peers, i, save_fig=False, show=True)
+    df_peers = pd.DataFrame({"Peer MSA Code": [str(x) for x in peers]})
+    df_peers["Peer Name"] = df_peers["Peer MSA Code"].apply(code2name)
     return df_peers
 
 
 def show_top_fms_peers(df_data, msa, year, n_peers, n_fms):
     peers, fms = find.get_top_n_fms_peers(df_data, msa, year, n_peers, n_fms)
     print(f"Comparison of {msa} and its peers for the 5 most present FMs")
-    for i in range(0, 5):
-        vis.quadrant_viz(df_data, msa, [msa] + peers, fms[i], save_fig=False, show=True)
-    df_peers = pd.DataFrame({"MSA": [str(x) for x in peers]})
-    df_peers["Name"] = df_peers["MSA"].apply(code2name)
+    for i in fms:
+        vis.quadrant_viz(df_data, msa, [msa] + peers, i, save_fig=False, show=True)
+    df_peers = pd.DataFrame({"Peer MSA Code": [str(x) for x in peers]})
+    df_peers["Peer Name"] = df_peers["Peer MSA Code"].apply(code2name)
+    return df_peers
+
+
+def show_coverage_peers(df_data, msa, year, n_peers, coverage):
+    coverage = coverage/10
+    print(coverage)
+    peers, fms = find.get_emp_threshold_peers(df_data, msa, year, n_peers, coverage)
+    print(fms)
+    for i in fms:
+        vis.quadrant_viz(df_data, msa, [msa] + peers, i, save_fig=False, show=True)
+    df_peers = pd.DataFrame({"Peer MSA Code": [str(x) for x in peers]})
+    df_peers["Peer Name"] = df_peers["Peer MSA Code"].apply(code2name)
     return df_peers
